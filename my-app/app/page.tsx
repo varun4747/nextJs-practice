f7787eb587223aa5420d546d637cd54d2aafbd4f
varun4747 <varunpapanaboina4747@gmail.com>
2026-03-12 10:57:56 +0530

    worked on tailwind css

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,31 @@
-export default function(){
-  return(
-    <div>
-      <h3>this is home page</h3>
+export default function Card() {
+  return (
+    <>
+    <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-MidnightBlueGray p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      
+      <img
+        className="size-12 shrink-0"
+        src="/img/logo.svg"
+        alt="ChitChat Logo"
+      />
+
+      <div>
+        <div className="text-xl font-medium text-black dark:text-white">
+          ChitChat
+        </div>
+
+        <p className="text-gray-500 dark:text-gray-400">
+          You have a new message!
+        </p>
+      </div>
+
+
     </div>
+    <button className="bg-sky-500 hover:bg-sky-700 text-white px-4 py-2 rounded">
+  Save changes
+</button>
+
+    </>
+    
   );
 }
